9d52f7fce99a557cf943855a302d985ffdc4e3bd
Varun Gowda G N <varungowdagn14@gmail.com>
2026-07-23 22:10:36 +0530

    Add initial implementation of bus.java

---
+++ bus.java
@@ -0,0 +1,109 @@
+Skip to content
+repositories Search Results · projects
+Filter by
+Languages
+Advanced
+11M results
+ (365 ms)
+11M results
+
+karan/Projects
+📃 A list of practical projects that anyone can solve in any programming language.
+47.8k
+·
+Updated on Aug 15, 2024
+
+bradtraversy/50projects50days
+50+ mini web projects using HTML, CSS & JS
+CSS
+·
+40.6k
+·
+Updated on Feb 26, 2025
+
+Python-World/python-mini-projects
+Public archive
+A collection of simple python mini projects to enhance your python skills
+python
+opensource
+tools
+projects
+learn-to-code
+Python
+·
+19.1k
+·
+Updated on Jul 17, 2022
+
+explosion/projects
+🪐 End-to-end NLP workflows from prototype to production
+nlp
+natural-language-processing
+annotations
+spacy
+datasets
+Python
+·
+1.4k
+·
+Updated on Oct 15, 2024
+
+ashishpatel26/500-AI-Agents-Projects
+The 500 AI Agents Projects is a curated collection of AI agent use cases across various industries. It showcases practical applications a…
+ai-agents
+genai
+Python
+·
+35k
+·
+Updated on Jun 6
+
+john-smilga/react-projects
+CSS
+·
+5.1k
+·
+Updated on Jul 24, 2024
+
+COGS108/Projects
+Final Project materials and description.
+python
+projects
+tutorials
+112
+·
+Updated on Dec 19, 2024
+
+NotHarshhaa/DevOps-Projects
+🚀 Real-world DevOps projects for aspiring engineers — Beginner to Advanced. Covers AWS, Kubernetes, Docker, CI/CD, Terraform, Jenkins, an…
+linux
+docker
+kubernetes
+learning
+jenkins
+Java
+·
+4.7k
+·
+Updated on May 2
+
+john-smilga/javascript-basic-projects
+CSS
+·
+3.9k
+·
+Updated on Aug 11, 2024
+
+udacity/ud120-projects
+Starter project code for students taking Udacity ud120
+DIGITAL Command Language
+·
+1.6k
+·
+Updated on Apr 23, 2024
+Package icon
+Sponsor open source projects you depend on
+Contributors are working behind the scenes to make open source better for everyone—give them the help and recognition they deserve.
+Explore sponsorable projects
+How can we improve search? 
+ProTip! Press the / key to activate the search input again and adjust your query.
